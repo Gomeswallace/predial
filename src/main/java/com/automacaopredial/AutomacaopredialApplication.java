7564a97336ef10ce2770cf8b607b2e4401c6c318
Wallace Gomes <gomes.wallace@hotmail.com>
2019-04-19 17:33:56 -0300

    Criado o projeto

---
+++ src/main/java/com/automacaopredial/AutomacaopredialApplication.java
@@ -0,0 +1,13 @@
+package com.automacaopredial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomacaopredialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomacaopredialApplication.class, args);
+	}
+
+}
