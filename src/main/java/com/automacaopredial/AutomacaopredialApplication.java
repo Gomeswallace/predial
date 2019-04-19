b041700ed509db34c4d031517cdb16591f9fddc1
Gomeswallace <gomes.wallace@hotmail.com>
2019-04-19 18:55:24 -0300

    Inserindo equipamentos e consultando pelo repository

--- src/main/java/com/automacaopredial/AutomacaopredialApplication.java
+++ src/main/java/com/automacaopredial/AutomacaopredialApplication.java
@@ -1,13 +1,34 @@
 package com.automacaopredial;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AutomacaopredialApplication {
+import com.automacaopredial.domain.Equipamento;
+import com.automacaopredial.domain.enums.TipoEquipamento;
+import com.automacaopredial.repositories.EquipamentoRepository;
 
+@SpringBootApplication
+public class AutomacaopredialApplication implements CommandLineRunner {
+
+	@Autowired
+	private EquipamentoRepository equipamentoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AutomacaopredialApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Equipamento ep1 = new Equipamento(1,"Equipamento 1", 2, true, TipoEquipamento.LAMPADA);
+		Equipamento ep2 = new Equipamento(2,"Equipamento 2", 3, true, TipoEquipamento.RADIO);
+		
+		equipamentoRepository.saveAll(Arrays.asList(ep1, ep2));
+	}
+
+	
 }
--- src/main/java/com/automacaopredial/resources/EquipamentoResource.java
+++ src/main/java/com/automacaopredial/resources/EquipamentoResource.java
@@ -1,8 +1,5 @@
 package com.automacaopredial.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.automacaopredial.domain.Equipamento;
-import com.automacaopredial.domain.enums.TipoEquipamento;
 import com.automacaopredial.services.EquipamentoService;
 
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -24,11 +20,7 @@ public class EquipamentoResource {
 	private EquipamentoService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) throws ObjectNotFoundException {
-				
-		Equipamento ep1 = new Equipamento(1,"Equipamento 1", 2, true, TipoEquipamento.LAMPADA);
-		Equipamento ep2 = new Equipamento(2,"Equipamento 2", 3, true, TipoEquipamento.RADIO);
-		
+	public ResponseEntity<?> find(@PathVariable Integer id) throws ObjectNotFoundException {				
 		Equipamento obj = service.find(id);
 		return ResponseEntity.ok().body(obj);		
 	}
