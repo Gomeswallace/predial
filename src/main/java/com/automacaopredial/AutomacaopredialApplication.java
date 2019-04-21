9ccf7c40b6dd89ed48ebef73b984b16c05b971b1
Gomeswallace <gomes.wallace@hotmail.com>
2019-04-21 18:57:51 -0300

    Ajuste nos endpoints para salvar no banco.

--- src/main/java/com/automacaopredial/AutomacaopredialApplication.java
+++ src/main/java/com/automacaopredial/AutomacaopredialApplication.java
@@ -37,24 +37,21 @@ public class AutomacaopredialApplication implements CommandLineRunner {
 		
 		Dispositivo disp1 = new Dispositivo(null, "Dispositivo 1", "TESTE", TipoDispositivo.ARDUINO_MEGA_2560);
 		Dispositivo disp2 = new Dispositivo(null, "Dispositivo 2", "TESTE 2", TipoDispositivo.ARDUINO_LALYPAD);
-		
-		dispositivoRepository.saveAll(Arrays.asList(disp1, disp2));
 
 		Ambiente amb1 = new Ambiente(null, "Garagem", "Entrada 1", disp1);
 		Ambiente amb2 = new Ambiente(null, "Area de serviço", "Lavanderia", disp2);
-				
-		ambienteRepository.saveAll(Arrays.asList(amb1, amb2));
 		
 		disp1.getAmbientes().addAll(Arrays.asList(amb1));
 		disp2.getAmbientes().addAll(Arrays.asList(amb2));
 		
 		Equipamento ep1 = new Equipamento(1,"Equipamento 1", 2, true, TipoEquipamento.LAMPADA, amb1);
 		Equipamento ep2 = new Equipamento(2,"Equipamento 2", 3, true, TipoEquipamento.RADIO, amb2);				
-		
-		equipamentoRepository.saveAll(Arrays.asList(ep1, ep2));
-				
+						
 		amb1.getEquipamentos().addAll(Arrays.asList(ep1));
 		amb2.getEquipamentos().addAll(Arrays.asList(ep2));
 		
+		dispositivoRepository.saveAll(Arrays.asList(disp1, disp2));
+		ambienteRepository.saveAll(Arrays.asList(amb1, amb2));
+		equipamentoRepository.saveAll(Arrays.asList(ep1, ep2));
 	}	
 }
